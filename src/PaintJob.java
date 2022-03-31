4d3ee1e8482fe798114f36b1e007b260b693503e
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-03-31 14:45:03 +0200

    Add getBucketCount(getBucketCount(double, double, double, int)

---
+++ src/PaintJob.java
@@ -0,0 +1,23 @@
+public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        double area = width * height;
+        double missingArea = area - areaPerBucket * extraBuckets;
+
+        if(missingArea <= 0) {
+            return 0;
+        } else {
+            return (int) Math.ceil(missingArea / areaPerBucket);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+    }
+}
